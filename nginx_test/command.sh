9a76bab3f7f68837655da9dcf1e788c267895977
EdwinGoo <egoomoy@gmail.com>
2024-01-18 21:14:36 +0900

    주석 추가

--- nginx_test/command.sh
+++ nginx_test/command.sh
@@ -18,7 +18,6 @@ kubectl port-forward nginx-test-565bf585d8-l7z75 8072:80 -n ever-ns
 kubectl delete namespace ever-ns       
 kubectl delete pv ever-nginx-nas-pv 
 kubectl get pv
-
 kubectl get all -n ever-ns
 
 ---
@@ -32,7 +31,8 @@ echo -n 'output/3/cat270.m3u8mysecret' | openssl md5 -hex
 ---
 # secure_link_md5 expression 의 경우
 date -d "today + 1 minutes" +%s
-echo -n "${expires} 127.0.0.1 mysecret" | openssl md5 -binary | openssl base64 | tr +/ -_ | tr -d =
-echo -n "1705580393 mysecret" | openssl md5 -binary | openssl base64 | tr +/ -_ | tr -d =
-http://localhost:8070/hls/0Ft4ijq-CUBfWtfFcgweYg/1705580393/output/3/mycat_720.m3u8
+echo -n "${expires} 127.0.0.1 mysecret" | openssl md5 -binary | openssl base64 | tr +/ -_ | tr -d = 
+# 127.0.0.1 을 적용하면 secure_link_md5 에 remote address를 사용해야하는데, 우선 제외 -> 아래로 처리
+echo -n "1705579995 mysecret" | openssl md5 -binary | openssl base64 | tr +/ -_ | tr -d =
+http://localhost:8070/hls/_nVbU71YeB8DnoBjm9kwow/1705579995/output/3/mycat_720.m3u8
 # if ($secure_link = "0") { return 410; } 만료 시 410 gone       
