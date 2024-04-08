b51f5325af9309063d57567031c7ef07c2512977
EdwinGoo <egoomoy@gmail.com>
2024-04-08 16:49:26 +0900

    connect 명령어 정리 중

--- kafka/command.sh
+++ kafka/command.sh
@@ -7,18 +7,21 @@ bin/kafka-server-start.sh config/server.properties
 cd next/mw/kafka_2.13-3.6.0/
 bin/connect-distributed ./etc/kafka/connect-distributed.properties
 
-#3. connect 실행 정상 확인, 관련 토픽 생성 확인
-cd next/mw/kafka_2.13-3.6.0/
-./bin/kafka-topics.sh --bootstrap-server localhost:9092 --list
 
-
-./bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic encoding_db_topic_test --from-beginning
+#3. mysql-connector 옮겨주기, 아래 경로로
+# 마리아db를 사용하지만, 커넥터는 mysql... 왜지
+# 우선 maria connect client 를 사용하면, upsert 로 sink가 안된다.
 /Users/plant/next/mw/confluent-7.3.1/share/java/kafka
+
+#4. connect 실행 정상 확인, 관련 토픽 생성 확인
+cd next/mw/kafka_2.13-3.6.0/
+./bin/kafka-topics.sh --create --topic encoding_db_topic_test --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1
+./bin/kafka-topics.sh --bootstrap-server localhost:9092 --list
+./bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic encoding_db_topic_test --from-beginning
 
 
 # confluent doc 에 옵션 참고 할 것 ***
 https://docs.confluent.io/kafka-connectors/jdbc/current/sink-connector/sink_config_options.html
-
 http://localhost:8083/connectors?expand=info&expand=status
 
 
