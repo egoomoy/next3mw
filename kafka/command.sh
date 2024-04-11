73904b019e3f0fc676316a3d60b69ee2898559d7
EdwinGoo <egoomoy@gmail.com>
2024-04-11 09:09:20 +0900

    카프카 정리 중

--- kafka/command.sh
+++ kafka/command.sh
@@ -1,24 +1,38 @@
-# 1. kafka 실행
+# 1. zookeeper & kafka
 cd next/mw/kafka_2.13-3.6.0/
 bin/zookeeper-server-start.sh config/zookeeper.properties
 bin/kafka-server-start.sh config/server.properties
 
-#2. connect 실행
-cd next/mw/kafka_2.13-3.6.0/
+#2. confluent connect
+# 다운로드 : https://www.confluent.io/ko-kr/previous-versions/
+cd next/mw/confluent-7.5.3/
 bin/connect-distributed ./etc/kafka/connect-distributed.properties
 
-
-#3. mysql-connector 옮겨주기, 아래 경로로
+#3-1. 드라이버용 : mysql-connector 옮겨주기, 아래 경로로
+# 다운로드 : https://mvnrepository.com/artifact/mysql/mysql-connector-java
 # 마리아db를 사용하지만, 커넥터는 mysql... 왜지
 # 우선 maria connect client 를 사용하면, upsert 로 sink가 안된다.
-/Users/plant/next/mw/confluent-7.3.1/share/java/kafka
+/Users/plant/next/mw/confluent-7.5.3/share/java/kafka 로 복-붙
 
-#4. connect 실행 정상 확인, 관련 토픽 생성 확인
+#3-2. JDBC Connector (Source and Sink) 다운로드
+# 다운로드 : https://www.confluent.io/hub/confluentinc/kafka-connect-jdbc
+
+#4. plugin path 설정
+cd /Users/edwin/next/mw/confluent-7.5.3/etc/kafka/connect-distributed.properties
+plugin.path=/usr/share/java, /Users/edwin/next/mw/confluentinc-kafka-connect-jdbc-10.7.6/lib
+
+#5. confluent connect 재기동
+bin/connect-distributed ./etc/kafka/connect-distributed.properties
+
+#6. connect 실행 정상 확인, 관련 토픽 생성 확인
 cd next/mw/kafka_2.13-3.6.0/
 ./bin/kafka-topics.sh --create --topic encoding_db_topic_test --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1
 ./bin/kafka-topics.sh --bootstrap-server localhost:9092 --list
 ./bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic encoding_db_topic_test --from-beginning
 
+
+
+# ---------------------------------------------------------------------------------------------------------------
 
 # confluent doc 에 옵션 참고 할 것 ***
 https://docs.confluent.io/kafka-connectors/jdbc/current/sink-connector/sink_config_options.html
